79061f72780b716bd2634946a39ec0047aac9be6
fotis alfieris <fotis@farmvent.com>
2022-05-18 19:34:39 +0300

    first

--- main2.py
+++ main2.py
@@ -4,7 +4,13 @@ from adc import ADC
 
 adc = ADC
 
-while True:
 # Read channel 0(Slot A0) voltage
-    print(adc.read_voltage(0))
-    time.sleep(1)
+print(adc.read_voltage(0))
+time.sleep(1)
+
+
+def read_ph(): 
+    import adc 
+
+address = 0x04
+addr = address
